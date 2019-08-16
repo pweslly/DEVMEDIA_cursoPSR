e409c7b1749e256d1e68f3c6f88125e1c6d72fc1
pablo <pablo_weslly@hotmail.com>
2019-08-16 09:21:17 -0300

    #1 - Usando metodo StudlyCaps

---
+++ ClassName.php
@@ -0,0 +1,29 @@
+<?php
+/* As classes devem ser escritas formato StudlyCaps, ou seja
+iniciar com letra maiúscula. */
+
+namespace Vendor\Package;
+
+class ClassName
+{
+  public function aVeryLongMethodName(
+    classTypeHint $ar1,
+    $arg2,
+      array $arg3 = []
+  ){
+    //method body
+  }
+
+  /* Os metodos devem ser declarados em camelCase, ou seja, devem inic
+  */
+
+ 
+    public function fooBarBaz($arg1, $arg2, $arg3 = [])
+    {
+      // method body
+    }
+  
+    
+}
+
+?>
